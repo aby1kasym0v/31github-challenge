30368f2a5fba271982bf64bea02e79f5b0eb6760
Nurik Ablazov <ablazovnurik99@gmail.com>
2025-02-10 23:17:41 +0300

    class

--- 8Day/class.py
+++ 8Day/class.py
@@ -1,4 +1,29 @@
-1
 def cast_spell():
     return  'Expecto Patronum'
 print(type(cast_spell))
+
+
+
+
+class Sword:
+
+    
+    def __init__(self, name, material, blade_length, grip):
+
+        self.name = name
+        self.blade_length = blade_length
+        self.material = material
+        self.grip = grip
+        print(f'Новый меч {name} выкован!')
+
+    def slashing_blow(self):
+        return (f'Нанесён рубящий удар мечом {self.name}. '
+                f'Радиус поражения: {self.blade_length}.')
+    
+    def piercing_strike(self):
+        return (f'Нанесён пронзающий удар мечом {self.name}. '
+                f'Рукоять {self.grip} мягко легла в руку.')
+
+    def sharpen(self):
+        return (f'Меч "{self.name}" заточен,'
+                f' {self.material} отлично поддалась обработке.') 
